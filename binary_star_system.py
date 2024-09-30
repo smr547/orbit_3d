ec2e48e4d91a95199f844cad4919fc6272e23673
Steven Ring <smr@southsky.com.au>
2024-10-01 01:38:05 +0300

    Throttle the display to a fixed frame rate

--- binary_star_system.py
+++ binary_star_system.py
@@ -3,7 +3,7 @@ import os
 import time
 from solar_system_3d import SolarSystem, Sun, Planet
 
-frame_rate = 20
+frame_rate = 15
 frame_interval = 1 / frame_rate
 report_rate_frames = frame_rate
 
@@ -37,17 +37,39 @@ def dump_results():
 def display_results():
     while True:
 
+        start_time = time.time()  # 
         solar_system.read_positions_from_pipe(r)
         solar_system.draw_all()
         solar_system.draw_all_bodies()
-        print(solar_system.bodies[0].position)
+        
+        # throttle this loop to the frame rate
+        display_time = time.time() - start_time 
+        sleep_time = frame_interval - display_time 
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+        else:
+            actual_frame_rate = frame_rate
+            if sleep_time != 0:
+                actual_frame_rate = 1/display_time
+            print("WARNING: frame rate ", int(actual_frame_rate), " try for ", frame_rate)
+
 
 
 def compute_results():
+    
     while True:
+        # start_time = time.time()  # 
         solar_system.calculate_all_body_interactions()
         solar_system.move_all()
         solar_system.write_positions_to_pipe(w)
+
+        # throttle this loop to the frame rate
+
+        # sleep_time = frame_interval - (time.time() - start_time) 
+        # if sleep_time > 0:
+        #    time.sleep(sleep_time)
+
+        
 
 
 # solar system and display properties have been intialised...
@@ -59,12 +81,12 @@ pid = os.fork()
 if pid:  # parent does the displaying 
    os.close(w) 
    r = os.fdopen(r) 
-   print ("Parent is displaying results ") 
+   print ("Parent is displaying results pid ", os.getpid()) 
+   print ("Child is computing results for ", len(solar_system.bodies), " bodies. pid: ", pid) 
    display_results()
    #dump_results()
 
 else:  # child does the computation 
    os.close(r) 
    w = os.fdopen (w, 'w') 
-   print ("Child is computing results for ", len(solar_system.bodies), " bodies") 
    compute_results()
