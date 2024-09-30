18bba33ab9e7d1c5a566f25af971f7bbf3470625
Steven Ring <smr@southsky.com.au>
2024-09-30 19:43:03 +0300

    Programable frame rate

--- binary_star_system.py
+++ binary_star_system.py
@@ -1,8 +1,13 @@
 # binary_star_system.py
 
+import time
 from solar_system_3d import SolarSystem, Sun, Planet
 
-solar_system = SolarSystem(400, projection_2d=True)
+frame_rate = 20
+frame_interval = 1 / frame_rate
+report_rate_frames = frame_rate
+
+solar_system = SolarSystem(800, projection_2d=True)
 
 suns = (
     Sun(solar_system, position=(40, 40, 40), velocity=(6, 0, 6)),
@@ -24,7 +29,29 @@ planets = (
     ),
 )
 
+spawn_time = time.time()
+frames_since_last_report = 0
+total_sleep_time = 0
+sleeps = 0
+
 while True:
+    frame_start_time = time.time()
+
     solar_system.calculate_all_body_interactions()
     solar_system.update_all()
     solar_system.draw_all()
+
+    loop_time = time.time() - frame_start_time
+    if loop_time < frame_interval:
+        sleep_time = frame_interval - loop_time
+        time.sleep(sleep_time)
+        total_sleep_time += sleep_time
+        sleeps += 1
+
+    frames_since_last_report += 1
+    if frames_since_last_report >= report_rate_frames:
+        idle_percent = total_sleep_time * 100 / (frame_interval * report_rate_frames)
+        print(int(idle_percent), " %idle CPU", end="\r")
+        frames_since_last_report = 0
+        total_sleep_time = 0
+        sleeps = 0
